78d7854093ac10bfdf95de58112149b4f27f369b
Subh <subhaushsingh@gmail.com>
2025-04-26 15:55:06 +0530

    fixed password issu

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
 
     if(!user) return <h1>Not logged in...</h1>
 
-    
+    if(!user.password) return <h2>password field is empty</h2>
 
   return (
     <><h1>Profile: {user.username}</h1></>
