4d6faff89b82dfff8081bef9d138709453cf3f51
Subh <subhaushsingh@gmail.com>
2025-04-27 16:57:21 +0530

    Initial commit: toggle theme & card creation

---
+++ light-dark-theme/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <Card></Card>
+
+    </>
+  )
+}
+
+export default App
