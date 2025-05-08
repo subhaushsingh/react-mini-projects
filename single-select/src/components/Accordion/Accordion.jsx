036baa8b9946df1ff1b53393b8d2c8e7a5d77508
Subh <subhaushsingh@gmail.com>
2025-05-08 20:19:27 +0530

    implemented single selection

--- single-select/src/components/Accordion/Accordion.jsx
+++ single-select/src/components/Accordion/Accordion.jsx
@@ -5,15 +5,32 @@ function Accordion() {
 
   const [selected,setSelected] = useState(null);
 
+
+  function handleSingleSelection(getCurrentId){
+    setSelected(getCurrentId ===  selected ? null : getCurrentId);
+  }
+
   return (
     <>
       <div className='wrapper'>
         <div className='accordion'>          
           {
             data && data.length > 0
-             ?  (<div>
-              
-            </div>)
+             ?  (
+              data.map((dataItem)=>(
+                <div className='item'>
+                  <div className='title' onClick={()=>{handleSingleSelection(dataItem.id)}}>
+                    <h3>{dataItem.question}</h3>
+                    <span>+</span>
+                  </div>
+                  {
+                    selected === dataItem.id ? 
+                    <div className='content'>{dataItem.answer}</div>
+                    : null
+                  }
+                </div>
+              ))
+              )
              :  (<div>No Data Found!</div>)
           }
         </div>
