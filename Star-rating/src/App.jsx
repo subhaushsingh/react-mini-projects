b8069ef28f63972ba062d349e1e164213342fd1e
Subh <subhaushsingh@gmail.com>
2025-05-14 09:22:30 +0530

    implemented onclick,mouseEnter,mouseLeave function

--- Star-rating/src/App.jsx
+++ Star-rating/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-
+import { use, useState } from 'react'
 import './App.css'
+import { FaStar } from 'react-icons/fa'
 
-function App({noOfStars}) {
- 
+function App({noOfStars=5}) {
+ const [rating,SetRating] = useState(0);
+ const [hoverStar, SetHoverStar] = useState(0);
+
+
+ function handleClick(currentIndex){
+    SetRating(currentIndex)
+ }
+
+ function handleMouseEnter(currentIndex){
+  SetHoverStar(currentIndex)
+ }
+
+ function handleMouseLeave(currentIndex){
+  SetHoverStar(rating)
+ }
 
   return (
     <>
       <div className='container'>
-        [...Array(noOfStars)]
+        {
+        [...Array(noOfStars)].map((_,i)=>{
+          i=i+1;
+
+            return <FaStar 
+              key={i}
+              onClick={()=>{handleClick(i)}}
+              onMouseEnter={()=>{handleMouseEnter(i)}}
+              onMouseLeave={()=>{handleMouseLeave(i)}}
+              size={40}
+              className={i <= (hoverStar||rating) ? 'star' : 'inactive'}
+            />
+        })
+        }
       </div>
     </>
   )
